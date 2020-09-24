b947d74e7269631f0e4df431f4fca8dd5753ce47
jinxin <jinxin@megvii.com>
2020-09-24 23:03:37 +0800

    add execute PPL

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -3,16 +3,19 @@
 @Version: v0.0
 @Author: jinxin@megvii.com
 @Time: 2020-09-19 22:06
-@Description: 
-Given an array of integers (nums) and an integer (target), return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+@Description:
+Given an array of integers (nums) and an integer (target), return indices of
+the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may
+not use the same element twice.
 You can return the answer in any order.
 """
 
 
 def two_sum(nums, target):
     """Find the indices of the two numbers in a list that add up to the target
-    This implementation uses the 'in' statement for iterable object and the index method for list object.
+    This implementation uses the 'in' statement for iterable object and
+    the index method for list object.
 
     Args:
         nums (List[int]): a list of integers
@@ -22,12 +25,14 @@ def two_sum(nums, target):
 
     """
     for i in range(len(nums)):
-        if target-nums[i] in nums[i+1:]:
-            return [i, i+1+nums[i+1:].index(target-nums[i])]
+        if target - nums[i] in nums[i + 1 :]:
+            return [i, i + 1 + nums[i + 1 :].index(target - nums[i])]
+
 
 def two_sum_by_hashmap(nums, target):
     """Find the indices of the two numbers in a list that add up to the target
-    This implementation uses the dict object as hashmap to reduce the query time.
+    This implementation uses the dict object as hashmap to reduce
+    the query time.
 
     Args:
         nums (List[int]): a list of integers
@@ -38,8 +43,8 @@ def two_sum_by_hashmap(nums, target):
     """
     hashmap = dict()
     for i in range(len(nums)):
-        if target-nums[i] in hashmap:
-            return [hashmap[target-nums[i]], i]
+        if target - nums[i] in hashmap:
+            return [hashmap[target - nums[i]], i]
         else:
             hashmap[nums[i]] = i
 
@@ -50,10 +55,16 @@ if __name__ == "__main__":
     target = 18
     print(two_sum(nums, target))
 
-    # test time
-    # from timeit import Timer
-    # exe_num = 20000000
-    # func_name = "two_sum_by_hashmap"
-    # t1 = Timer("%s(nums, target)" % func_name,
-    #         "from __main__ import %s; nums=%s; target=%s" % (func_name, nums, target))
-    # print("Executing func %s %d times costs %.3f second!" % (func_name, exe_num, t1.timeit(number=exe_num)))
+    # test the running time
+    from timeit import Timer
+
+    exe_num = 20000000
+    func_name = "two_sum_by_hashmap"
+    t1 = Timer(
+        "%s(nums, target)" % func_name,
+        "from __main__ import %s; nums=%s; target=%s" % (func_name, nums, target),
+    )
+    print(
+        "Executing func %s %d times costs %.3f second!"
+        % (func_name, exe_num, t1.timeit(number=exe_num))
+    )
